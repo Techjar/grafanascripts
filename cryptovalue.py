11698f18c7d96233b0f517ca4c3349d34735f37c
Techjar <tecknojar@gmail.com>
2018-03-10 15:13:54 -0500

    haha oops

--- cryptovalue.py
+++ cryptovalue.py
@@ -111,6 +111,6 @@ while True:
 	update_value('bbs', 'BBS', get_info_crex24, 60)
 	update_value('xao', 'XAO', get_info_tradeogre, 30)
 	update_value('grft', 'GRFT', get_info_tradeogre, 30)
-	update_value('btcp', 'BTCP', get_info_cmc, 30)
+	update_value('btcp', 'bitcoin-private', get_info_cmc, 30)
 
 	time.sleep(1)
