5047f8f95c4b2295ec801805011c8ee3dd55a3fb
Techjar <tecknojar@gmail.com>
2018-04-09 01:51:06 -0400

    Ok fine, also include 1h average in db as 'hashrate_avg'

--- xmrigproxy.py
+++ xmrigproxy.py
@@ -8,11 +8,11 @@ while True:
 
 	query = ""
 	for worker in data['workers']:
-		query += worker[0] + " hashrate=" + str(worker[9] * 1000) + ",shares=" + str(worker[3]) + ",hashes=" + str(worker[6]) + "\n"
+		query += worker[0] + " hashrate=" + str(worker[9] * 1000) + ",hashrate_avg=" + str(worker[10] * 1000) + ",shares=" + str(worker[3]) + ",hashes=" + str(worker[6]) + "\n"
 
 	response = requests.get("http://" + cfg.xmrigproxy_address)
 	data = json.loads(response.text)
-	query += cfg.xmrigproxy_total_name + " hashrate=" + str(data['hashrate']['total'][1] * 1000) + ",shares=" + str(data['results']['accepted']) + ",hashes=" + str(data['results']['hashes_total']) + "\n"
+	query += cfg.xmrigproxy_total_name + " hashrate=" + str(data['hashrate']['total'][1] * 1000) + ",hashrate_avg=" + str(data['hashrate']['total'][2] * 1000) +  ",shares=" + str(data['results']['accepted']) + ",hashes=" + str(data['results']['hashes_total']) + "\n"
 
 	requests.post(url = "http://localhost:8086/write?db=minerhash", data = query.encode('utf-8'), headers = {'Content-Type': 'application/octet-stream'})
 	print(query)
